3ddb28cd627f96efb7543829108a0229ce1a1bc5
harshkr212 <guptagamer8@gmail.com>
2026-06-29 11:00:59 +0530

    Setup Nextapp,file and folder structure,auth pages

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Authform from '@/components/Authform'
+const page = () => {
+  return (
+    <div>
+      <Authform type="sign-in" />
+    </div>
+  )
+}
+
+export default page
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Authform from '@/components/Authform'
+const page = () => {
+  return (
+    <div>
+      <Authform type="sign-up" />
+    </div>
+  )
+}
+
+export default page
