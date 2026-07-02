013731b23f661ee0f84e88e812f693aafc6671bb
harshkr212 <guptagamer8@gmail.com>
2026-07-02 12:49:14 +0530

    Switched to jwks-rsa,jose firebase

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint:{
-    ignoreDuringBuilds:true
+ typescript: {
+    ignoreBuildErrors: true
   },
-  typescript:{
-    ignoreBuildErrors:true
-  }
-} as any;
+};
 
 
 export default nextConfig;
